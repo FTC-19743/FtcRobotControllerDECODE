ae1b89509bf8baebba8f2353adc6f30324018666
ftc19743 <90291326+ftc19743@users.noreply.github.com>
2025-09-20 17:07:21 -0700

    Further Shooter Testing Further Intake Testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testcode/BallDetector.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testcode/BallDetector.java
@@ -141,6 +141,7 @@ public class BallDetector extends LinearOpMode
 
 
 
+
             telemetry.update();
 
         }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testcode/ShooterTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testcode/ShooterTest.java
@@ -6,21 +6,28 @@ import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.teamcode.libs.teamUtil;
 
 @Config
-@TeleOp(name = "Motor Test", group = "Test Code")
-public class MotorTest extends LinearOpMode {
+@TeleOp(name = "Shooter Test", group = "Test Code")
+public class ShooterTest extends LinearOpMode {
 
     public static double PowerIncrement = 0.1;
     public static double VelocityIncrement = 100;
+    public static double SmallVelocityIncrement  = 10;
     public static final float openPos = 0.39f;
     public static final float launchPos = 0.51f;
     public double CurrentPower = 0;
     public double currentVelocity = 0;
+    public static double shooterIncrement = 0.11;
+    public static double shooterP = 50;
+    public static double shooterI = 1;
+    public static double shooterD = 0.5;
+    public double shooterF = 0;
 
 
 
@@ -34,6 +41,9 @@ public class MotorTest extends LinearOpMode {
         DcMotorEx motorL = hardwareMap.get(DcMotorEx.class,"motorL");
         DcMotorEx motorR = hardwareMap.get(DcMotorEx.class,"motorR");
 
+
+
+
         Servo pusher = hardwareMap.get(Servo.class,"pusher");
 
         teamUtil.init(this);
@@ -44,7 +54,10 @@ public class MotorTest extends LinearOpMode {
             return;
         }
         pusher.setPosition(openPos);
+        teamUtil.log("PIDF coefficients: " + motorL.getPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER));
         while (opModeIsActive()) {
+            motorL.setVelocityPIDFCoefficients(shooterP, shooterI, shooterD, shooterF);
+            motorR.setVelocityPIDFCoefficients(shooterP, shooterI, shooterD, shooterF);
             motor.setPower(CurrentPower);
             //motorL.setPower(CurrentPower);
             //motorR.setPower(-CurrentPower);
@@ -58,10 +71,19 @@ public class MotorTest extends LinearOpMode {
                 CurrentPower-=PowerIncrement;
                 currentVelocity-=VelocityIncrement;
             }
-            if(gamepad1.aWasReleased()) {
-                pusher.setPosition(launchPos);
-                teamUtil.pause(400);
-                pusher.setPosition(openPos);
+            if(gamepad1.dpadRightWasReleased()){
+                CurrentPower+=PowerIncrement;
+                currentVelocity+=SmallVelocityIncrement;
+            }
+            if(gamepad1.dpadLeftWasReleased()){
+                CurrentPower-=PowerIncrement;
+                currentVelocity-=SmallVelocityIncrement;
+            }
+            if(gamepad1.aWasPressed()) {
+                pusher.setPosition(pusher.getPosition()-shooterIncrement);
+            }
+            if(gamepad1.yWasPressed()) {
+                pusher.setPosition(pusher.getPosition()+shooterIncrement);
             }
 
             TelemetryPacket packet = new TelemetryPacket();
@@ -69,6 +91,9 @@ public class MotorTest extends LinearOpMode {
             telemetry.addLine("Current Power: " + CurrentPower);
             telemetry.addLine("currentVelocity: " + currentVelocity);
             telemetry.addLine("ReportedVelocity: " + motorL.getVelocity()+", "+motorR.getVelocity());
+            telemetry.addData("Reported Left Velocity: " , motorL.getVelocity());
+            telemetry.addData("Reported Right Velocity: " , motorR.getVelocity());
+
             telemetry.update();
         }
     }
